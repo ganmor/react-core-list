45d4297555b36e33d8ad80c0835b6d1a925cbcf4
Morgan <mlaupies@evenium.com>
2015-03-06 17:36:45 +0100

    setup

--- src/scripts/examples/ReactInfiniteListAppExample.js
+++ src/scripts/examples/ReactInfiniteListAppExample.js
@@ -9,15 +9,46 @@ require('../../styles/main.css');
 
 var infiniteList = require('../react-infinite-list');
 
-var ReactInfiniteListApp = React.createClass({
-  render: function() {
-    return (
-      <div className='main'>
-          TEst
-      </div>
-    );
-  }
-});
-React.render(<ReactInfiniteListApp />, document.getElementById('content')); // jshint ignore:line
+var Person =  React.createClass({
 
-module.exports = ReactInfiniteListApp;
+componentWillMount : function () {
+  this.color = this.props.element.id%2==0 ? '#cbcbcb' :'#e3eaee';
+},
+
+render : function () {
+
+  var height = 75 + Math.random()* 40;
+
+  return (<div className="item-line-style" style={{background:this.color, borderBottom:'1px solid silver'}}>
+            <div>
+              <img src={this.props.element.picture} width="55"  style={{verticalAlign:'middle'}} />
+              <span style={{paddingLeft:'25px'}}><strong>{this.props.element.name} </strong> - {this.props.element.company}</span>
+              <div>{this.props.element.about}</div>
+            </div>
+
+          </div>);
+}
+});
+
+
+var PeopleList = React.createClass({
+
+  render : function () {
+
+    var elements = _.map(this.props.jsonList, function (element) {
+      return (<Person element={element} key={element.id} />);
+    });
+
+    return (
+      <ReactInfiniteList>
+        {elements}
+      </ReactInfiniteList>
+    );
+
+  }
+
+});
+
+React.render(<PeopleList />, document.getElementById('content')); // jshint ignore:line
+
+module.exports = PeopleList;
