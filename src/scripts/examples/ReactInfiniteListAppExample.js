207b631fdbcc5103eaca403a3623330322afe9c2
Morgan <mlaupies@evenium.com>
2015-03-12 17:14:14 +0100

    add shuffle button to example

--- src/scripts/examples/ReactInfiniteListAppExample.js
+++ src/scripts/examples/ReactInfiniteListAppExample.js
@@ -10,15 +10,22 @@ require('../../styles/main.css');
 var ReactInfiniteList = require('../react-infinite-list');
 var _ = require('underscore');
 
-var Person =  React.createClass({
 
-  componentWillMount : function () {
-    this.color = this.props.element.id%2==0 ? '#cbcbcb' :'#e3eaee';
-  },
+var UpdateButton = React.createClass({
+  render : function () {
+    return (
+      <button onClick={this.props.shuffleColors}> Shuffle colors</button>
+    );
+  }
+
+});
+
+var Person =  React.createClass({
 
   render : function () {
 
     var height = 75 + Math.random()* 40;
+    this.color = this.props.element.id%2==0 ? this.props.colors[0] : this.props.element.id%3==0 ?  this.props.colors[1] :  this.props.colors[2] ;
 
     return (<div className="item-line-style" style={{background:this.color, borderBottom:'1px solid silver'}}>
               <div>
@@ -31,18 +38,37 @@ var Person =  React.createClass({
   });
 
 
-var PeopleList = React.createClass({
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
+var PeopleList = React.createClass({
+  getInitialState : function () {
+    return { colors : ['#2a3fe5', '#949DEA', 'white']};
+  },
+  shuffleColors : function () {
+    this.setState({
+      colors : [getRandomColor(), getRandomColor(), getRandomColor()]
+    });
+  },
   render : function () {
 
     var elements = _.map(this.props.jsonList, function (element) {
-      return (<Person element={element} key={element.id} />);
-    });
+      return (<Person element={element} key={element.id} colors={this.state.colors} />);
+    }, this);
 
     return (
-      <ReactInfiniteList>
-        {elements}
-      </ReactInfiniteList>
+      <div>
+        <UpdateButton shuffleColors={this.shuffleColors}/>
+        <ReactInfiniteList>
+          {elements}
+        </ReactInfiniteList>
+      </div>
     );
   }
 
