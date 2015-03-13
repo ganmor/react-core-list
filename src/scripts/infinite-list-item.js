467a3ae988ad72400148584d4e2284dfe5fd3441
Morgan <mlaupies@evenium.com>
2015-03-13 15:53:58 +0100

    no needed anymore

--- src/scripts/infinite-list-item.js
+++ src/scripts/infinite-list-item.js
@@ -12,32 +12,6 @@ var React = require('react'),
     getInitialState : function () {
       return {};
     },
-
-    // Cache the height of the element
-    componentDidMount : function () {
-      if (this.getDOMNode()) {
-        this.state.cachedHeight = this.getDOMNode().offsetHeight;
-      }
-    },
-
-    componentDidUpdate : function () {
-      if (this.getDOMNode()) {
-        this.state.cachedHeight = this.getDOMNode().offsetHeight;
-      }
-    },
-
-    shouldComponentUpdate : function (nextProps, nextState) {
-
-      var renderStatusChanged, shouldComponentUpdate;
-      if (!this.props) {
-        return true;
-      }
-
-      shouldComponentUpdate = (this.props.rendered != nextProps.rendered) || nextProps.rendered;
-
-      return shouldComponentUpdate;
-    },
-
     render : function () {
 
       var style;
